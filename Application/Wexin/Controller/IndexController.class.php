6431567ee7dc8c31d6424bd516c9ebc2c1fa08fb
codeGlider <hjw2012607@icloud.com>
2016-04-15 21:28:43 +0800

    继续测试

--- Application/Wexin/Controller/IndexController.class.php
+++ Application/Wexin/Controller/IndexController.class.php
@@ -19,13 +19,14 @@ class IndexController extends Controller {
         //用code换取access_token
         $URL="https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appId&secret=$appsercret&code=$code&grant_type=authorization_code";
         $access_token_json = json_decode(httpGet($URL),true);
+        echo $access_token_json;
         if (isset($access_token_json['access_token'])) {
           $access_token = $access_token_json['access_token'];
              $openid = $access_token_json['openid'];
              $userInfoURL = "https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid";
               $userInfoJson = json_decode(httpGet($userInfoURL),true);
 
-              echo $userInfoJson;
+              //echo $userInfoJson;
 
         }else{
           echo "NO ACCESS_TOKEN";
