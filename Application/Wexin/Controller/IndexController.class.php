9ec819a43d05d11c0dd7291d2b50e655fa32786b
codeGlider <hjw2012607@icloud.com>
2016-04-15 20:43:50 +0800

    测试微信oAuth网页认证

--- Application/Wexin/Controller/IndexController.class.php
+++ Application/Wexin/Controller/IndexController.class.php
@@ -11,86 +11,35 @@ class IndexController extends Controller {
           $wechatObj->responseMsg();
       }
     }
+    public function oAuth(){
+      //获得code
+      if (isset($_GET['code'])){
+
+        $code = $_GET['code'];
+        //用code换取access_token
+        $URL="https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appId&secret=$appsercret&code=$code&grant_type=authorization_code"
+        $access_token_json = json_encode(httpGet($URL));
+        if (isset($access_token_json['access_token'])) {
+          $access_token = $access_token_json['access_token'];
+             $openid = $access_token_json['openid'];
+             $userInfoURL = "https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid";
+              $userInfoJson = json_encode(httpGet($userInfoURL));
+
+              echo $userInfoJson;
+
+        }else{
+          echo "NO ACCESS_TOKEN";
+        }
+        //用access_token获取用户信息
+
+        }else{
+    echo "NO CODE";
+        }
+
+    }
 
 }
 
 
 
-class wechatCallbackapiTest
-{
-	public function valid()
-    {
-        $echoStr = $_GET["echostr"];
-
-        //valid signature , option
-        if($this->checkSignature()){
-        	echo $echoStr;
-        	exit;
-        }
-    }
-
-    public function responseMsg()
-    {
-		//get post data, May be due to the different environments
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
-      	//extract post data
-		if (!empty($postStr)){
-                /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-                   the best way is to check the validity of xml by yourself */
-                libxml_disable_entity_loader(true);
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-                $textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
-							</xml>";
-				if(!empty( $keyword ))
-                {
-              		$msgType = "text";
-                	$contentStr = "Welcome to WeMedical!";
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
-                }else{
-                	echo "Input something...";
-                }
-
-        }else {
-        	echo "";
-        	exit;
-        }
-    }
-
-	private function checkSignature()
-	{
-        // you must define TOKEN by yourself
-        if (!defined("TOKEN")) {
-            throw new Exception('TOKEN is not defined!');
-        }
-
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-
-		$token = TOKEN;
-		$tmpArr = array($token, $timestamp, $nonce);
-        // use SORT_STRING rule
-		sort($tmpArr, SORT_STRING);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
-
-		if( $tmpStr == $signature ){
-			return true;
-		}else{
-			return false;
-		}
-	}
-}
 ?>
