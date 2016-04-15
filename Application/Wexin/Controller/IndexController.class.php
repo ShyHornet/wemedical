1978144cc2b4d227442cdf18ffab7d05d1fc0996
codeGlider <hjw2012607@icloud.com>
2016-04-15 20:55:23 +0800

    修改bug

--- Application/Wexin/Controller/IndexController.class.php
+++ Application/Wexin/Controller/IndexController.class.php
@@ -17,13 +17,13 @@ class IndexController extends Controller {
 
         $code = $_GET['code'];
         //用code换取access_token
-        $URL="https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appId&secret=$appsercret&code=$code&grant_type=authorization_code"
-        $access_token_json = json_encode(httpGet($URL));
+        $URL="https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appId&secret=$appsercret&code=$code&grant_type=authorization_code";
+        $access_token_json = json_decode(httpGet($URL));
         if (isset($access_token_json['access_token'])) {
           $access_token = $access_token_json['access_token'];
              $openid = $access_token_json['openid'];
              $userInfoURL = "https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid";
-              $userInfoJson = json_encode(httpGet($userInfoURL));
+              $userInfoJson = json_decode(httpGet($userInfoURL));
 
               echo $userInfoJson;
 
