2ea28eb8408e85522a5a63fa9b8b997666285d30
ShyHornet(Aliyun) <hjw2012607@163.com>
2016-05-30 10:09:29 +0800

    完成客户端通知的显示和删除

--- Application/Help/Controller/NoticeController.class.php
+++ Application/Help/Controller/NoticeController.class.php
@@ -3,6 +3,17 @@ namespace Help\Controller;
 use Think\Controller;
 class NoticeController extends Controller {
     public function index(){
+       $Notice = M('Notice');
+       $list = $Notice->order('notice_date desc')->select();
+       $this->assign('noticelist',$list);
         $this->show();
+    }
+    public function delNotice(){
+      $notice = M('Notice');
+    if($notice->where('notice_id='+$_POST['notice_id'])->delete()=='0'){
+      echo "1";
+    }else{
+      echo "0";
+    }
     }
 }
